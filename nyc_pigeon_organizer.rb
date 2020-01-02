ee9184e0e4b326aaf5567ff3ecefc29f70457e17
Miwha Geschwind <miwhag@gmail.com>
2020-01-02 00:18:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {
+    "Theo" => {},
+    "Peter Jr." => {},
+    "Lucky" => {}, 
+    "Ms. K" => {}, 
+    "Queenie" => {},
+    "Andrew" => {}, 
+    "Alex" => {}
+  }
 end
